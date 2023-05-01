d4f0ffff2538fbba898fd24e88af41366146f120
Andrew <code70@inbox.ru>
2023-05-01 19:35:29 +0700

    503 maintance style

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="robots" content="noindex,nofollow">
+    <title>Weiss Studio - Томск</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+    <style>
+        .service{
+            min-height: 100vh;
+            display: flex;
+            justify-content: center;
+            align-content: center;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <div class="row service">
+            <div class="col-12 col-md-6 col-lg-6">
+                <object data="/assets/icons/logo-text.svg" type=""></object>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
