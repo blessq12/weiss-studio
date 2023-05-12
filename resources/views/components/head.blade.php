d982f268b6ff0033cafb63c76c576362b08f03c9
Andrew <code70@inbox.ru>
2023-05-12 09:04:21 +0700

    asd

--- resources/views/components/head.blade.php
+++ resources/views/components/head.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="RU-ru">
 <head>
+   <meta name="robots" content="noindex,nofollow">
   <title>{{ $title ?? 'Заголовок не задан' }} - {{ env('APP_NAME')}} </title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
