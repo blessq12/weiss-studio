02d0612229912311b5f7bb90005b5ae9e1719e4b
Andrew <code70@inbox.ru>
2023-06-12 00:17:38 +0700

    metrika counter

--- resources/views/components/head.blade.php
+++ resources/views/components/head.blade.php
@@ -16,6 +16,23 @@
   <link rel="stylesheet" href="{{ asset('/assets/css/owl.theme.css')}}">
   <link rel="stylesheet" href="{{ asset('/assets/css/style.css')}}{{ '?ver='.env('ASSET_VERSION') ?? "ver=1.0" }}">
   <link rel="stylesheet" href="{{ asset('/assets/css/user.css')}}{{ '?ver='.env('ASSET_VERSION') ?? "ver=1.0" }}">
+   <!-- Yandex.Metrika counter -->
+      <script type="text/javascript" >
+         (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+         m[i].l=1*new Date();
+         for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+         k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+         (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+         ym(93926164, "init", {
+            clickmap:true,
+            trackLinks:true,
+            accurateTrackBounce:true,
+            webvisor:true
+         });
+      </script>
+      <noscript><div><img src="https://mc.yandex.ru/watch/93926164" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+   <!-- /Yandex.Metrika counter -->
 </head>
 <body id="index" class="wide_layout position-relative">
   <div class="loader">
